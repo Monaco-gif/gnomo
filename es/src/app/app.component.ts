93b7c607fd5bd070f06e559d8047473210eedae1
Monaco-gif <monaco.matteo@iisgalvanimi.edu.it>
2021-05-02 21:33:56 +0000

    fileJJson

---
+++ es/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Vegetali } from './model/vegetables.model';
+//import { Cart } from './model/cart_product.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'es';
+  obsVeg : Observable<Vegetali[]>
+  listaVeg : Vegetali[];
+  constructor(private http: HttpClient){}
+
+  details: Vegetali = new Vegetali("#","#","#");
+
+  //cart_product: Cart[] = new Array<Cart>();
+
+  ngOnInit(): void {
+    this.obsVeg = this.http.get<Vegetali[]>('https://my-json-server.typicode.com/malizia-g/fine_anno_exp/verdure');
+    this.obsVeg.subscribe(this.getVeg);
+  }
+
+  getVeg = (data: Vegetali[]) => {
+    this.listaVeg = data;
+    console.log(this.listaVeg);
+  }
+
+}
---
+++ es/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+//import { VegetableListComponent } from './vegetable-list/vegetable-list.component';
+//import { DetailsComponent } from './details/details.component';
+//import { CarrelloComponent } from './carrello/carrello.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    //VegetableListComponent,
+    //DetailsComponent,
+    //CarrelloComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule // nel caso non funzionasse ricordarsi di inserirlo
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
